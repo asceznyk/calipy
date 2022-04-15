a143fcd3fa1f9447d11ba5dce68aa79c3c93bb88
asceznyk <asceznyk@gmail.com>
2022-04-15 18:39:33 +0530

    ..

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ def main(train_dir):
     imgs, labels = load_img_vector_pairs(train_dir)
     print(labels[0])
     plt.imshow(imgs[0], interpolation='nearest')
-    plt.show()
+    plt.savefig('00.png')
 
 if __name__ == '__main__':
     main(sys.argv[1])
