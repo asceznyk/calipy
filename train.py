5adeed916b329506accc65867b8192f924de0d71
asceznyk <asceznyk@gmail.com>
2022-04-15 18:04:28 +0530

    ..

--- train.py
+++ train.py
@@ -1,13 +1,15 @@
 import os
 import sys
-import argparse
 import numpy as np
+
+import matplotlib.pyplot as plt 
 
 from utils import *
 
 def main(train_dir):
     imgs, labels = load_img_vector_pairs(train_dir)
-    print(imgs.shape, labels.shape)
+    plt.imshow(imgs[0], interpolation='nearest')
+    plt.savefig('00.png')
 
 if __name__ == '__main__':
     main(sys.argv[1])
