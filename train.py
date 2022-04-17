4d2e0d64f05e1fd7f350e75f9ff1a859315acbc4
asceznyk <asceznyk@gmail.com>
2022-04-17 17:43:54 +0530

    ..

--- train.py
+++ train.py
@@ -10,6 +10,11 @@ from utils import *
 
 def main(train_dir):
     imgs, labels = load_img_vector_pairs(train_dir)
+    data = CalibData(imgs, labels)
+
+    print(data[0])
+
+    #loader = DataLoader()
 
 if __name__ == '__main__':
     main(sys.argv[1])
--- utils.py
+++ utils.py
@@ -2,6 +2,20 @@ import os
 import glob
 import numpy as np
 
+import torch
+
+from torch.utils.data import Dataset, DataLoader
+
+class CalibData(Dataset):
+    def __init__(self, imgs, labels):
+        self.imgs = imgs
+        self.labels = labels
+
+    def __getitem__(self, i):
+        img = torch.tensor(self.imgs[i]/255.0).float()
+        label = self.labels[i]
+        return img, label
+
 def load_img_vector_pairs(_dir):
     os.chdir(_dir)
     labels = []
@@ -15,7 +29,6 @@ def load_img_vector_pairs(_dir):
             imgs.extend(np.load(open(f'{_dir}/{file}' , 'rb')))
 
     return np.array(imgs), np.array(labels)
-
 
 
 
