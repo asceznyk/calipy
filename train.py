03bffcea076de0e8ab3f414d04382a3f7014ecdd
asceznyk <asceznyk@gmail.com>
2022-04-18 13:00:09 +0530

    ..

--- train.py
+++ train.py
@@ -12,11 +12,12 @@ from model import *
 def main(train_dir):
     batch_size = 8
     
-    imgs, labels = load_img_vector_pairs(train_dir)
-    img_size = imgs.shape[1:]
-    label_size = labels.shape[1:]
+    imgs, labels = load_img_vector_pairs(train_dir) 
 
     data = CalibData(imgs, labels)
+    img, label = data[0]
+    img_size = img.shape[1:]
+    label_size = label.shape[1:]
     loader = DataLoader(data, batch_size=batch_size, shuffle=True)
 
     x, _ = next(iter(loader))
