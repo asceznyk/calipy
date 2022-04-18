6aa1c5c3677f3240b1bb98d5390950162ddb2a7f
asceznyk <asceznyk@gmail.com>
2022-04-18 14:12:36 +0530

    ..

--- model.py
+++ model.py
@@ -24,7 +24,7 @@ class CalibNet(nn.Module):
             self.linear_block(200, 100),
             self.linear_block(100, 50),
             self.linear_block(50, 10),
-            self.linear_block(10, ang_dim[0])
+            nn.Linear(10, ang_dim[0])
         )
 
         self._w = None
