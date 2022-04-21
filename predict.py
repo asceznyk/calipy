b5c2bfdedec5bae82a4561c3f0697ba4396f362c
asceznyk <asceznyk@gmail.com>
2022-04-21 19:16:47 +0530

    ..

--- predict.py
+++ predict.py
@@ -21,9 +21,10 @@ def main(args):
         model.load_state_dict(torch.load(args.ckpt_path))
     model.eval()
 
+    os.chdir(data_dir)
+    
     print(glob.glob(ext))
 
-    os.chdir(data_dir)
     for video_path in glob.glob(ext):
         cap = cv2.VideoCapture(video_path)
         ret = True
