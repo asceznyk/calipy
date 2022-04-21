aef4611bb3d631fe7e679276a43f58e1d20e5fd1
asceznyk <asceznyk@gmail.com>
2022-04-21 17:58:43 +0530

    ..

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ from torch.utils.data import Dataset, DataLoader
 
 max_scale = 10
 img_size = (3, 200, 266)
-label_size = 2
+label_size = (2,)
 
 class CalibData(Dataset):
     def __init__(self, imgs, labels):
