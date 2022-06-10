6edf6ee4835bbe5e14ecd5e0f4d2a2becbe70ef8
asceznyk <asceznyk@gmail.com>
2022-06-10 11:29:39 +0530

    ..

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ def fit(model, train_loader, valid_loader=None, ckpt_path=None, epochs=10, lr=0.
 
 
 def main(args):
-    torch.manual_seed()
+    torch.manual_seed(0)
 
     batch_size = args.batch_size
     main_dir = args.main_dir
