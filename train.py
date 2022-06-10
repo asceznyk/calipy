6d9e0e5499e0f4528b1c4b47557640bcaef9ee33
asceznyk <asceznyk@gmail.com>
2022-06-10 11:44:18 +0530

    ..

--- train.py
+++ train.py
@@ -103,7 +103,7 @@ def main(args):
 
     #single_batch = DataLoader(CalibData(imgs_train[random_idx:random_idx+batch_size], labels_train[random_idx:random_idx+batch_size]), batch_size=batch_size)
 
-    single_batch = DataLoader(CalibData(np.zeros((batch_size, *img_size)), labels_train[random_idx: random_idx+batch_size]), batch_size=batch_size)
+    single_batch = DataLoader(CalibData(np.zeros((batch_size, (200, 266, 3))), labels_train[random_idx: random_idx+batch_size]), batch_size=batch_size)
     fit(model, single_batch, epochs=args.epochs)
 
 if __name__ == '__main__':
