5a62e695bee71630e8afe2b1835c0530840b6297
asceznyk <asceznyk@gmail.com>
2022-06-20 20:39:56 +0530

    resize var

--- preprocess.py
+++ preprocess.py
@@ -17,14 +17,14 @@ def main(videos_dir, resize, ext='*.hevc'):
             ret, img = cap.read() 
             if ret:
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                if int(resize):
+                if resize:
                     img = cv2.resize(img, dsize=(img_size[2], img_size[1]), interpolation=cv2.INTER_CUBIC)
                 frames.append(img)
                 
         np.save(f'{video_path}.npy', np.stack(frames, axis=0))
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], int(sys.argv[2]))
 
 
 
